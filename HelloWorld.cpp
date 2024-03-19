33d32d5a7f0df8ddfbb5a7f71497c270c199c663
omarjokers7 <omarjokers54@gmail.com>
2024-03-19 20:41:01 +0300

    initial commit

---
+++ HelloWorld.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+vector<string> sayilar;
+int rastsayi(int minV, int maxV)
+{
+int sayi = rand() % (maxV- mivV + 1) minV;
+return sayi;
+}
+int main()
+{
+    srand(time(0));
+    int sayisiniri = 1000000;
+   for (int i = 0; i < sayisiniri; i++)
+   {
+    if ()
+    {
+        sayilar.push_back(i);
+    }
+    
+   }
+   
+
+
+
+}
